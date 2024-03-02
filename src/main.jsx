528d0952faf15e8fe44366df2016c87776cfdc6c
許曄 <123572897+xue2227@users.noreply.github.com>
2024-03-03 00:14:28 +0800

    vercel speed testing
    addind vercel speed test

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <SpeedInsights>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </SpeedInsights>,
 )
 const loader = document.querySelector('.loader');
 if (loader) {
